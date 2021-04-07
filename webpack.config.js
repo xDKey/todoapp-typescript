b95eb527ab5777fab365d90f1426924160f7afdc
Vladimir Mironov <DKey656@gmail.com>
2021-04-07 12:20:52 +0300

    build: add base template and settings for webpack with react

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, './public'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './public'),
+  },
+}
