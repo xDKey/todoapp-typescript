b2ada3cda0b09dc06cd496f71fe128ac763e951e
Vladimir Mironov <vladimir.mironov@jetruby.com>
2021-04-14 18:19:34 +0300

    test(App): add unit-tests

--- src/tests/App.spec.tsx
+++ src/tests/App.spec.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
 import { mount } from '@cypress/react'
+import React from 'react'
 import App from '../components/App'
 
 it('<App />', () => {
   mount(<App />)
-  cy.contains('ToDo List')
+  cy.waitForReact()
+
+  cy.react('App')
+    .get('form')
+    .within(() => {
+      cy.get('input').type('Check')
+      cy.get('select').select('Work')
+      cy.get('button').click()
+    })
+  cy.react('App').should('contain', 'Check')
+
+  cy.react('App')
+    .get('section > div')
+    .last()
+    .within(() => {
+      cy.get('button').click()
+    })
+
+  cy.react('App')
+    .should('not.contain', 'Check')
+    .and('contain', 'Write first task')
+
+  cy.react('App')
+    .contains('Write first task')
+    .click()
+    .should('have.css', 'text-decoration', 'line-through solid rgb(0, 0, 0)')
+    .click()
+    .and('have.css', 'text-decoration', 'none solid rgb(0, 0, 0)')
 })
